50bd5c3878e35d8fb84d2aa8f8e47c6e3f5497ac
Wes Wilson <wes@iamweswilson.com>
2017-08-13 11:17:43 -0700

    Coming soon site

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.BB_ACCESS_TOKEN = 'a7cb7f6c03f88535cb12787206d4cce7f4248e138e586275ae97dc7a999dd90d';
+config.BB_SITE_ID = 'TBD.bitballoon.com';
+config.BB_SITE_DIR = '_site';
+
+
+module.exports = config;
